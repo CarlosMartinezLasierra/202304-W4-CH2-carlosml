f50532364373dce7b1a7a3d256d62fea184ce490
Carlos Martinez <carlosmartinezlasierra@gmail.com>
2023-05-23 19:00:36 +0200

    Add function into Repeatpasword

---
+++ src/components/form/Tasks/task2/RepeatPasWord.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+interface RepeatPasswordInputProps {
+  onChange: (repeatPassword: string) => void;
+}
+
+const RepeatPasswordInput: React.FC<RepeatPasswordInputProps> = ({
+  onChange,
+}) => {
+  const [repeatPassword, setRepeatPassword] = useState("");
+
+  const handleRepeatPasswordChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRepeatPassword(e.target.value);
+    onChange(e.target.value);
+  };
+
+  return (
+    <div>
+      <label htmlFor="repeatPassword">Repeat Password:</label>
+      <input
+        type="password"
+        id="repeatPassword"
+        value={repeatPassword}
+        onChange={handleRepeatPasswordChange}
+        required
+      />
+    </div>
+  );
+};
+
+export default RepeatPasswordInput;
